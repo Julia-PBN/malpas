d64d7044e474a915bef2d08f4467c932e6466d24
Julia-PBN <julia.phambanien@gmail.com>
2023-07-20 15:06:37 +0200

    add basic reading file to string for lexing

--- src/main.ml
+++ src/main.ml
@@ -1,1 +1,15 @@
-let () = print_endline "Malpas ne veut pas compiler, méchant."
+let file_to_string file =
+  let s = really_input_string file (in_channel_length file) in 
+    close_in file;
+    s
+
+let read_all file_name = 
+  file_to_string (open_in file_name)
+
+
+let main () =
+  let len = (Array.length Sys.argv) in
+  let argv = (Array.sub Sys.argv 1 (len-1)) in (* skip argv0 *)
+    Array.iter (fun x -> print_string (read_all x)) argv
+
+let _ = main ()
